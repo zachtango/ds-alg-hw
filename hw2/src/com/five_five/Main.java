80766400f09658ebab6be6ab9fcedd29cf73ba49
Zach Tang <zachtangoj@gmail.com>
2022-10-19 22:50:01 -0500

    ignore unecessary files

--- hw2/src/com/five_five/Main.java
+++ hw2/src/com/five_five/Main.java
@@ -1,34 +1,103 @@
 package com.five_five;
+import java.io.*;
+import java.util.*;
+
+/**
+ * Write a short description of the bug in this code and a possible fix. The
+ * online judge throws a runtime error.
+ *
+ * Problem description:
+ *
+ * You are given a 2-dimensional array of 1s and 0s.
+ *
+ * If a cell contains a 1, then there is an obstacle in that cell and you
+ * cannot walk over it. Find the shortest path from the top left corner of the
+ * grid to the bottom right. You can only move in the four cardinal directions.
+ * It is guaranteed that the top left and bottom right are both 0.
+ *
+ * If there's no path from the top left to the bottom right, print -1.
+ *
+ * Examples:
+ * Input:
+ * 4 4
+ * 0 1 1 1
+ * 0 0 1 1
+ * 1 0 0 1
+ * 1 0 0 0
+ *
+ * Output:
+ * 6
+ *
+ * Input:
+ * 3 4
+ * 0 1 0 0
+ * 0 1 0 0
+ * 1 0 0 0
+ *
+ * Output:
+ * -1
+ */
 
 public class Main {
+    private static int[] dx = {1, 0, -1, 0};
+    private static int[] dy = {0, 1, 0, -1};
 
-    public static void main(String[] args) {
-        SeparateChainingHashTable<Integer> table = new SeparateChainingHashTable<>(12);
+    public static class State {
+        int x, y, moves;
 
-        // inserting elements into hash table
-        // table size = nextPrime(12) = 13
-        table.insert(10);
-        table.insert(11);
-        table.insert(33); // collides with 10 --> added to chain at the hash index
-        table.insert(36);
-        table.insert(21);
-        table.insert(5);
-        table.insert(6);
+        public State(int x, int y, int moves) {
+            this.x = x;
+            this.y = y;
+            this.moves = moves;
+        }
+    }
 
-        // test insert
-        table.print();
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int m = scan.nextInt();
 
-        table.remove(10);
-        table.remove(33);
-        table.remove(5);
-        table.remove(21);
+        boolean[][] grid = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                grid[i][j] = scan.nextInt() == 1;
+            }
+        }
 
-        // test remove
-        table.print();
+        Queue<State> queue = new LinkedList<>();
+        queue.add(new State(0, 0, 0));
 
-        System.out.println("Table contains 10?: " + table.contains(10));
-        System.out.println("Table contains 2?: " + table.contains(2));
-        System.out.println("Table contains 1?: " + table.contains(1));
-        System.out.println("Table contains 11?: " + table.contains(11));
+        boolean solved = false;
+        // boolean arrays default to false
+        boolean[][] seen = new boolean[n][m];
+
+        while (!queue.isEmpty()) {
+            State state = queue.remove();
+
+            if (state.x == n - 1 && state.y == m - 1) {
+                System.out.println(state.moves);
+                solved = true;
+                break;
+            }
+
+            System.out.println(state.x + " " + state.y);
+            // Don't continue if we've been down this path or it's blocked
+            if (seen[state.x][state.y] || grid[state.x][state.y]) {
+                continue;
+            }
+
+            seen[state.x][state.y] = true;
+
+            for (int d = 0; d < 4; d++) {
+                int newX = state.x + dx[d];
+                int newY = state.y + dy[d];
+
+                queue.add(new State(newX, newY, state.moves + 1));
+            }
+        }
+
+        if (!solved) {
+            System.out.println(-1);
+        }
     }
 }
